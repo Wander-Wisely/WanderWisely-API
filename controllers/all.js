4f201a2932c2bdeaeec9212cf6be6e83af031f7d
AAAdzim <adzim.al.islami@gmail.com>
2023-06-09 13:57:26 +0700

    Update all.js

--- controllers/all.js
+++ controllers/all.js
@@ -1,13 +1,20 @@
 const models = require("../models");
 
 const getAlldatas = async (req, res) => {
+  const pageSize = 10;
+  const currentPage = req.query.page || 1;
+
   try {
-    // Fetch the Wisata data along with its associated Aktivitas
-    const wisataData = await models.TourismAttraction.findAll({
+    const offset = (currentPage - 1) * pageSize;
+    const limit = pageSize;
+
+    // Fetch the Wisata data along with its associated Aktivitas with pagination
+    const { rows, count } = await models.TourismAttraction.findAndCountAll({
+      offset,
+      limit,
       include: [
         {
           model: models.TourismType,
-          // as: "tourismType", // Specify the alias for the association
         },
         {
           model: models.TourismActivity,
@@ -21,7 +28,13 @@ const getAlldatas = async (req, res) => {
       ],
     });
 
-    res.json(wisataData);
+    const totalPages = Math.ceil(count / pageSize);
+
+    res.json({
+      currentPage,
+      totalPages,
+      items: rows,
+    });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Error retrieving data" });
@@ -38,7 +51,6 @@ const getAllDatasById = async (req, res) => {
       include: [
         {
           model: models.TourismType,
-          // as: "tourismType", // Specify the alias for the association
         },
         {
           model: models.TourismActivity,
@@ -64,3 +76,71 @@ const getAllDatasById = async (req, res) => {
 };
 
 module.exports = { getAlldatas, getAllDatasById };
+
+
+// const models = require("../models");
+
+// const getAlldatas = async (req, res) => {
+//   try {
+//     // Fetch the Wisata data along with its associated Aktivitas
+//     const wisataData = await models.TourismAttraction.findAll({
+//       include: [
+//         {
+//           model: models.TourismType,
+//           // as: "tourismType", // Specify the alias for the association
+//         },
+//         {
+//           model: models.TourismActivity,
+//         },
+//         {
+//           model: models.TourismFacility,
+//         },
+//         {
+//           model: models.TourismFile,
+//         },
+//       ],
+//     });
+
+//     res.json(wisataData);
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ message: "Error retrieving data" });
+//   }
+// };
+
+// const getAllDatasById = async (req, res) => {
+//   const wisataId = req.params.id; // Assuming the ID is passed as a route parameter
+
+//   try {
+//     // Fetch the Wisata data along with its associated Aktivitas based on the provided ID
+//     const wisataData = await models.TourismAttraction.findOne({
+//       where: { id: wisataId },
+//       include: [
+//         {
+//           model: models.TourismType,
+//           // as: "tourismType", // Specify the alias for the association
+//         },
+//         {
+//           model: models.TourismActivity,
+//         },
+//         {
+//           model: models.TourismFacility,
+//         },
+//         {
+//           model: models.TourismFile,
+//         },
+//       ],
+//     });
+
+//     if (!wisataData) {
+//       return res.status(404).json({ message: "Wisata not found" });
+//     }
+
+//     res.json(wisataData);
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ message: "Error retrieving data" });
+//   }
+// };
+
+// module.exports = { getAlldatas, getAllDatasById };
